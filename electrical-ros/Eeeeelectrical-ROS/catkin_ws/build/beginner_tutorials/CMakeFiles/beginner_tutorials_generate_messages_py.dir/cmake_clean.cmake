9813a012b8c414525c48db1f1d15e7e630122d0d
shainequigley <shainequigley>
2017-07-27 22:16:21 -0400

    Long story short, folders can do interesting things when you rename them to the same thing

---
+++ electrical-ros/Eeeeelectrical-ROS/catkin_ws/build/beginner_tutorials/CMakeFiles/beginner_tutorials_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/beginner_tutorials_generate_messages_py"
+  "/home/shaine/robot_lifeguard/github/electrical-ros/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/msg/_num.py"
+  "/home/shaine/robot_lifeguard/github/electrical-ros/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/srv/_AddTwoInts.py"
+  "/home/shaine/robot_lifeguard/github/electrical-ros/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/msg/__init__.py"
+  "/home/shaine/robot_lifeguard/github/electrical-ros/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/srv/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/beginner_tutorials_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
